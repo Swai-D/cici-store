8b60502de6c82d5b43d74def50ce0ec9a748e546
Swai-D <davyswai@gmail.com>
2025-07-05 12:56:15 +0300

    fix: revert to using only @vite directive for all environments (restores working production CSS)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,12 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @if (app()->environment('local') || app()->environment('development'))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @else
-            <link rel="stylesheet" href="{{ asset('build/assets/app-DP4bMnzw.css') }}">
-            <script src="{{ asset('build/assets/app-DNxiirP_.js') }}" defer></script>
-        @endif
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
